e76af5e65223686902891414c21a61d565a52f61
mxkx74 <0x66ccff@gmail.com>
2023-09-18 23:58:03 +0900

    next.configの修正

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  // production環境ではtestidを削除する
-  reactRemoveProperties:
-    process.env.NODE_ENV === "production"
-      ? { properties: ["^data-testid$"] }
-      : false,
-  // production環境ではerror以外のconsoleを削除する
-  removeConsole:
-    process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
-};
+  compiler: {
+    // production環境ではtestidを削除する
+    reactRemoveProperties:
+      process.env.NODE_ENV === 'production' ? { properties: ['^data-testid$'] } : false,
+    // production環境ではerror以外のconsoleを削除する
+    removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
