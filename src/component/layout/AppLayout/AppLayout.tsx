0ccfdc228991b1315feb70fa870495087ef220ad
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-11-23 12:40:07 +0900

    providerの追加

--- src/component/layout/AppLayout/AppLayout.tsx
+++ src/component/layout/AppLayout/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { type ReactNode } from 'react';
 import { Inter } from 'next/font/google';
+import { NextAuthProvider } from '@/provider/NextAuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,7 +11,9 @@ type Props = {
 export const AppLayout = ({ children }: Props) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthProvider>{children}</NextAuthProvider>
+      </body>
     </html>
   );
 };
