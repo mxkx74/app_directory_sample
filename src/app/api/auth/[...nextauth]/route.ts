38742248388981b0a1af7f5c0106db8331e7c375
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-11-30 17:29:08 +0900

    route handlerの設定

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/feature/auth/options';
+import { authOptions } from '@/feature/auth/setting';
 
 const handler = NextAuth(authOptions);
 
