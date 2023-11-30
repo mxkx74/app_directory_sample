6719d72dd28c7a4ef7fc721559feab853197f498
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-11-30 17:30:39 +0900

    repositoryの追加

---
+++ src/domain/repository/authRepository.ts
@@ -0,0 +1,39 @@
+import { signIn, signOut } from 'next-auth/react';
+import { path } from '@/constant/path';
+import { type AuthModel } from '@/domain/model/authModel';
+import { fetcher } from '@/lib/fetcher';
+
+export const authRepository = () => {
+  const endpoint = path.auth.token();
+
+  return {
+    // ユーザーがログインした後、callbackUrlにリダイレクトされます。
+    signIn(callbackUrl = '/') {
+      void signIn('spotify', { callbackUrl });
+    },
+
+    // ユーザーログアウトします。
+    signOut() {
+      void signOut();
+    },
+
+    // アクセストークンを更新し、更新されたトークンを返します。
+    async refreshAccessToken(refreshToken?: string) {
+      const body = new URLSearchParams({
+        client_id: process.env.SPOTIFY_CLIENT_ID as string,
+        client_secret: process.env.SPOTIFY_CLIENT_SECRET as string,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken ?? '',
+      });
+
+      const { payload } = await fetcher<AuthModel>(endpoint, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        method: 'POST',
+        cache: 'no-store',
+        body,
+      });
+
+      return payload;
+    },
+  };
+};
