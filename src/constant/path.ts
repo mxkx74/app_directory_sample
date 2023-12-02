caf46553ff8dbe168c0545575c66b3d1d112934b
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-02 14:07:34 +0900

    pathの修正

--- src/constant/path.ts
+++ src/constant/path.ts
@@ -1,3 +1,4 @@
+export const baseEndpoint = 'https://api.spotify.com/v1';
 export const authEndpoint = 'https://accounts.spotify.com/api/token';
 
 export const path = {
@@ -5,5 +6,10 @@ export const path = {
     token: () => `${authEndpoint}` as const,
     login: () => `${authEndpoint}/api/auth/login` as const,
     callback: () => `${authEndpoint}/api/auth/callback/spotify` as const,
+  },
+  user: {
+    me: () => `${baseEndpoint}/me` as const,
+    users: (userName?: string) => `${baseEndpoint}/users/${userName}` as const,
+    topArtists: () => `${baseEndpoint}/me/artists` as const,
   },
 } as const;
