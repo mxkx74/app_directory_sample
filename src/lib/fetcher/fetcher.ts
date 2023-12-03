eaa89fe0c8193600b4c9909b32eb8f8a42073579
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-03 20:20:30 +0900

    fetcherにvalidationErrorを追加

--- src/lib/fetcher/fetcher.ts
+++ src/lib/fetcher/fetcher.ts
@@ -1,16 +1,53 @@
+import { ZodError, type ZodObject } from 'zod';
 import { type keyPath } from '@/constant/path';
 import { type HttpResponse } from '@/type/httpResponse';
 import { HttpError } from '@/util/error';
 
-/** tsDoc
+/**
+ * @description バリデーションエラーをHttpResponseに変換する
+ * @param {unknown} validationError - バリデーションエラー
+ * @param {boolean} isThrowError - エラーをthrowするかどうか
+ * @returns {Promise<HttpResponse<T>>} - HttpResponse
+ * @throws {HttpError} - isThrowErrorがtrueの場合、HttpErrorをthrowする
+ * @throws {unknown} - validationErrorがZodError以外の場合、validationErrorをthrowする
+ */
+export const transformValidation = async <T>(validationError: unknown, isThrowError: boolean) => {
+  if (validationError instanceof ZodError) {
+    const error: HttpResponse<T> = {
+      payload: undefined,
+      error: validationError,
+      status: 400,
+    };
+
+    if (isThrowError) throw new HttpError(error);
+    return error;
+  }
+
+  throw validationError;
+};
+
+/**
  * @description fetcherのレスポンスをHttpResponseに変換する
  * @param {Response} response - fetchレスポンス
  * @param {boolean} isThrowError - 200 ~ 299以外のステータスコードの場合にエラーをthrowするかどうか
  * @returns {Promise<HttpResponse<T>>} - HttpResponse
  * @throws {HttpError} - isThrowErrorがtrueの場合、HttpErrorをthrowする
  */
-export const transformResponse = async <T>(response: Response, isThrowError: boolean): Promise<HttpResponse<T>> => {
+export const transformResponse = async <T>(
+  response: Response,
+  isThrowError: boolean,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  validationSchema?: ZodObject<any>,
+): Promise<HttpResponse<T>> => {
   const json = await response.json();
+
+  if (validationSchema != null) {
+    try {
+      validationSchema.parse(json);
+    } catch (error) {
+      return transformValidation(error, isThrowError);
+    }
+  }
 
   // isThrowErrorがtrueで200 ~ 299以外のステータスコードの場合はcatchで処理する
   if (!response.ok) {
@@ -19,9 +56,11 @@ export const transformResponse = async <T>(response: Response, isThrowError: boo
       error: json,
       status: response.status,
     };
+
     if (isThrowError) {
       throw new HttpError(data);
     }
+
     return data;
   }
 
@@ -32,7 +71,7 @@ export const transformResponse = async <T>(response: Response, isThrowError: boo
   };
 };
 
-/** tsDoc
+/**
  * @description fetcherのエラーをHttpResponseに変換する
  * @param {Error} error - fetcherのエラー
  * @param {boolean} isThrowError - エラーをthrowするかどうか
@@ -51,7 +90,7 @@ export const transformErrorResponse = async <T>(error: Error, isThrowError: bool
   throw error;
 };
 
-/** tsDoc
+/**
  * @description fetcher
  * @param {RequestInfo} input - fetchの第一引数
  * @param {RequestInit} init - fetchの第二引数
@@ -62,6 +101,8 @@ export const transformErrorResponse = async <T>(error: Error, isThrowError: bool
 export const fetcher = async <T>(
   input: RequestInfo,
   init?: RequestInit & { next?: { revalidate: number; tags?: keyPath[] } },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  validationSchema?: ZodObject<any>,
   isThrowError = false,
 ): Promise<HttpResponse<T>> => {
   return fetch(input, {
@@ -74,6 +115,6 @@ export const fetcher = async <T>(
       ...init?.next,
     },
   })
-    .then(async (response) => await transformResponse<T>(response, isThrowError))
+    .then(async (response) => await transformResponse<T>(response, isThrowError, validationSchema))
     .catch(async (error: Error) => await transformErrorResponse<T>(error, isThrowError));
 };
