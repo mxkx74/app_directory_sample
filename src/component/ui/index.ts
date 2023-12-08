cab49b02285d7acb6e70cb3b9b863fe875403805
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-08 23:49:00 +0900

    popoverの追加

--- src/component/ui/index.ts
+++ src/component/ui/index.ts
@@ -4,3 +4,4 @@ export * from './Input';
 export * from './Card';
 export * from './Stack';
 export * from './Avatar';
+export * from './Popover';
--- src/feature/me/component/MeNavigation.tsx
+++ src/feature/me/component/MeNavigation.tsx
@@ -1,6 +1,16 @@
 import { type ComponentPropsWithRef } from 'react';
 import { ArrowDownIcon, BellIcon } from '@radix-ui/react-icons';
-import { Avatar, AvatarFallback, AvatarImage, Button, Stack } from '@/component/ui';
+import Link from 'next/link';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  Stack,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/component/ui';
 import { meInteractor } from '@/feature/me/use-case/interactor';
 import { type MeViewModel } from '@/feature/me/use-case/outputBoundary';
 
@@ -24,12 +34,49 @@ export const MeNavigationPresentation = (props: PresentationProps) => {
         <Button variant="default" size="icon" className="w-8 h-8 rounded-full hover:scale-105">
           <BellIcon />
         </Button>
-        <Button size="icon" className="w-8 h-8 rounded-full hover:scale-105">
-          <Avatar size="sm">
-            <AvatarImage alt={props.display_name} src={avatarImage.url} />
-            <AvatarFallback>{props.display_name}</AvatarFallback>
-          </Avatar>
-        </Button>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button size="icon" className="w-8 h-8 rounded-full hover:scale-105">
+              <Avatar size="sm">
+                <AvatarImage alt={props.display_name} src={avatarImage.url} />
+                <AvatarFallback>{props.display_name}</AvatarFallback>
+              </Avatar>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent align="end" className="bg-slate-900 border-none w-auto p-1">
+            <ul role="menu">
+              <li role="presentation">
+                <Button asChild width="full" round="none" className="justify-start hover:bg-stone-900" role="menuitem">
+                  <Link href="/">アカウント</Link>
+                </Button>
+              </li>
+              <li role="presentation">
+                <Button asChild width="full" round="none" className="justify-start hover:bg-stone-900" role="menuitem">
+                  <Link href="/">プロフィール</Link>
+                </Button>
+              </li>
+              <li role="presentation">
+                <Button asChild width="full" round="none" className="justify-start hover:bg-stone-900" role="menuitem">
+                  <Link href="/">Premiumにアップグレード</Link>
+                </Button>
+              </li>
+              <Button
+                asChild
+                width="full"
+                round="none"
+                className="justify-start hover:bg-stone-900 border-b border-b-gray-500"
+                role="menuitem"
+              >
+                <Link href="/">設定</Link>
+              </Button>
+              <li role="presentation">
+                <Button asChild width="full" round="none" className="justify-start hover:bg-stone-900" role="menuitem">
+                  <Link href="/">ログイン</Link>
+                </Button>
+              </li>
+            </ul>
+          </PopoverContent>
+        </Popover>
       </Stack>
     )
   );
