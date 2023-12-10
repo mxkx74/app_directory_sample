f0966cdc95a44a028bf6e8ec5d29e89f2044054b
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-10 22:06:49 +0900

    httpエラーの修正

--- src/util/error.ts
+++ src/util/error.ts
@@ -1,7 +1,7 @@
 import { type HttpResponse } from '@/type/httpResponse';
 
 export class HttpError<T> extends Error {
-  readonly data?: HttpResponse<T>;
+  readonly status: number = 400;
 
   constructor(data: HttpResponse<T>) {
     const { message, name, stack, cause } = data.error ?? {};
@@ -9,6 +9,6 @@ export class HttpError<T> extends Error {
     this.name = name ?? 'HttpError';
     this.stack = stack;
     this.cause = cause;
-    this.data = data;
+    this.status = data.status;
   }
 }
