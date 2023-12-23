04eaa1bc3de4a9d1a6363dfb418031f9316451bc
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-23 14:57:52 +0900

    Refactor Home page form

--- src/component/page/Home/Home.tsx
+++ src/component/page/Home/Home.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Button, Input, Stack } from '@/component/ui';
-import { MeLibraryList } from '@/feature/meLibraryList/component/MeLibraryList';
 import styles from './Home.module.scss';
 
 export const HomePage = () => {
   return (
-    <>
-      <MeLibraryList />
-      <form className={styles.form}>
-        <Stack direction="row" className={styles.search}>
-          <Input type="text" />
-          <Button type="submit">Submit</Button>
-        </Stack>
-      </form>
-    </>
+    <form className={styles.form}>
+      <Stack direction="row" className={styles.search}>
+        <Input type="text" />
+        <Button type="submit">Submit</Button>
+      </Stack>
+    </form>
   );
 };
