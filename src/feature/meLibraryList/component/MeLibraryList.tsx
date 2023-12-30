774fcd4455ed311c15f1163289daf5aa00666f11
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-30 22:45:34 +0900

    Remove unused import statement in MeLibraryList.tsx

--- src/feature/meLibraryList/component/MeLibraryList.tsx
+++ src/feature/meLibraryList/component/MeLibraryList.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 import InfiniteScroll from 'react-infinite-scroller';
 import { Avatar, AvatarImage, Stack, Text, AvatarFallback } from '@/component/ui';
 import { type MeLibraryViewModel, useMeLibraryList } from '@/feature/meLibraryList/use-case';
-import {} from '@/feature/meLibraryList/use-case';
 import * as styles from './MeLibraryList.module.scss';
 
 type PresentationProps = ComponentPropsWithRef<'div'> & {
