69646ed9150a0e6a9a7c152b20a299da7855b617
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-30 16:46:12 +0900

    Refactor Sidebar component and add scroll functionality
    This commit refactors the Sidebar component by adding scroll functionality to the MeLibraryList module. It also includes the use of the ErrorBoundary component for error handling.

--- src/component/ui-group/Sidebar/Sidebar.tsx
+++ src/component/ui-group/Sidebar/Sidebar.tsx
@@ -1,21 +1,29 @@
 import { Suspense } from 'react';
+import { getServerSession } from 'next-auth';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Stack } from '@/component/ui';
 import { GlobalNav } from '@/component/ui-group';
+import { authOptions } from '@/feature/auth/setting';
 import { MeLibraryList } from '@/feature/meLibraryList/component/MeLibraryList';
 import styles from './Sidebar.module.scss';
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+  const token = session?.access_token ?? '';
+
   return (
     <nav>
       <Stack space="S">
-        <Stack width="full" space="M" className={styles.sidebarSub}>
+        <Stack space="M" className={styles.sidebarSub}>
           <GlobalNav />
         </Stack>
 
-        <Stack width="full" className={styles.sidebarMain}>
-          <Suspense fallback={<>loading</>}>
-            <MeLibraryList />
-          </Suspense>
+        <Stack className={styles.sidebarMain}>
+          <ErrorBoundary fallback={<div>error</div>}>
+            <Suspense fallback={<>loading</>}>
+              <MeLibraryList token={token} />
+            </Suspense>
+          </ErrorBoundary>
         </Stack>
       </Stack>
     </nav>
