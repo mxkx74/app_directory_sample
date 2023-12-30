4e386822f683d3cbd36db0788534b3c6bb316ca6
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-30 19:34:36 +0900

    Refactor MeLibraryList component and add infinite scrolling

--- src/feature/meLibraryList/component/MeLibraryList.tsx
+++ src/feature/meLibraryList/component/MeLibraryList.tsx
@@ -1,56 +1,74 @@
+'use client';
+
 import { Fragment, type ComponentPropsWithRef } from 'react';
-
 import Link from 'next/link';
-import { getServerSession } from 'next-auth';
+import InfiniteScroll from 'react-infinite-scroller';
 import { Avatar, AvatarImage, Stack, Text, AvatarFallback } from '@/component/ui';
-import { authOptions } from '@/feature/auth/setting';
-import { meLibraryInteractor } from '@/feature/meLibraryList/use-case';
-import { type MeLibraryViewModel } from '@/feature/meLibraryList/use-case';
-
+import { type MeLibraryViewModel, useMeLibraryList } from '@/feature/meLibraryList/use-case';
+import {} from '@/feature/meLibraryList/use-case';
 import * as styles from './MeLibraryList.module.scss';
 
-type PresentationProps = ComponentPropsWithRef<'div'> & MeLibraryViewModel;
+type PresentationProps = ComponentPropsWithRef<'div'> & {
+  loadMore: () => Promise<void>;
+  hasMore: boolean;
+  items: MeLibraryViewModel['items'];
+};
 
 // presentation component
-export const MeLibraryListPresentation = ({ items, ...props }: PresentationProps) => {
+export const MeLibraryListPresentation = ({ loadMore, hasMore, items, ...props }: PresentationProps) => {
   return (
-    <Stack direction="column" space="none" {...props} className={styles.wrapper}>
-      {items.map((item) => {
-        const [image] = item.images ?? [];
-        const rounded = item.type === 'playlist' ? 'full' : 'md';
-        const type = item.type === 'playlist' ? 'プレイリスト' : 'アルバム';
-        const owner = item.type === 'playlist' ? `・${item.owner}` : '';
+    <Stack direction="column" space="none" className={styles.wrapper} {...props}>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={() => {
+          void loadMore();
+        }}
+        initialLoad={false}
+        hasMore={hasMore}
+        useWindow={false}
+        className={styles.infiniteScroll}
+      >
+        {items.map((item, index) => {
+          const [image] = item.images ?? [];
+          const rounded = item.type === 'playlist' ? 'full' : 'md';
+          const type = item.type === 'playlist' ? 'プレイリスト' : 'アルバム';
+          const owner = item.type === 'playlist' ? `・${item.owner}` : '';
 
-        return (
-          <Fragment key={item.id}>
-            <Link href="/" className={styles.link}>
-              <Stack width="full" direction="row" space="M" padding="S">
-                <Avatar size="md" rounded={rounded}>
-                  {image && <AvatarImage alt={item.name} src={image?.url} />}
-                  <AvatarFallback>{item.name?.substring(0, 2).toUpperCase()}</AvatarFallback>
-                </Avatar>
-                <Stack direction="column" space="none" width="full" className={styles.description}>
-                  <Text overflow="ellipsis" className={styles.title}>
-                    {item.name}
-                  </Text>
-                  <Text overflow="ellipsis" size="S">{`${type} ${owner}`}</Text>
+          return (
+            <Fragment key={index}>
+              <Link href="/" className={styles.link}>
+                <Stack width="full" direction="row" space="M" padding="S">
+                  <Avatar size="md" rounded={rounded}>
+                    {image && <AvatarImage alt={item.name} src={image?.url} />}
+                    <AvatarFallback>{item.name?.substring(0, 2).toUpperCase()}</AvatarFallback>
+                  </Avatar>
+                  <Stack direction="column" space="none" width="full" className={styles.description}>
+                    <Text overflow="ellipsis" className={styles.title}>
+                      {item.name}
+                    </Text>
+                    <Text overflow="ellipsis" size="S">{`${type} ${owner}`}</Text>
+                  </Stack>
                 </Stack>
-              </Stack>
-            </Link>
-          </Fragment>
-        );
-      })}
+              </Link>
+            </Fragment>
+          );
+        })}
+      </InfiniteScroll>
     </Stack>
   );
 };
 
 // container component
-export const MeLibraryList = async () => {
-  const { access_token: token } = (await getServerSession(authOptions)) ?? {};
-  if (!token) return null;
+export const MeLibraryList = ({ token }: { token: string }) => {
+  const { data: result, fetchNextPage, hasNextPage } = useMeLibraryList({ token });
 
-  const { payload, error } = await meLibraryInteractor.findAllLibrary(token);
-  if (error) throw error;
-
-  return payload && <MeLibraryListPresentation {...payload} />;
+  return (
+    <MeLibraryListPresentation
+      loadMore={async () => {
+        void fetchNextPage();
+      }}
+      hasMore={hasNextPage}
+      items={result?.pages ?? []}
+    />
+  );
 };
