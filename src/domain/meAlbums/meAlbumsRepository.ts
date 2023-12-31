0a7e8c39c0430c6df51f865ffd6852142f95aaf0
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-31 20:29:24 +0900

    Refactor meRepository, meAlbumsRepository, and mePlaylistsRepository

--- src/domain/meAlbums/meAlbumsRepository.ts
+++ src/domain/meAlbums/meAlbumsRepository.ts
@@ -8,57 +8,55 @@ import {
   type MeAlbumsContainModel,
 } from './meAlbumsModel';
 
-export const meAlbumsRepository = () => {
-  const endpoint = path.user.meAlbums;
+const endpoint = path.user.meAlbums;
 
-  return {
-    async find(token: string, params?: { limit: number; offset: number }, isThrowError = false) {
-      const url = pathBuilder(endpoint, params);
-      return fetcher<MeAlbumsModel>(url, undefined, {
-        validationSchema: meAlbumsModelSchema,
+export const meAlbumsRepository = {
+  async find(token: string, params?: { limit: number; offset: number }, isThrowError = false) {
+    const url = pathBuilder(endpoint, params);
+    return fetcher<MeAlbumsModel>(url, undefined, {
+      validationSchema: meAlbumsModelSchema,
+      isThrowError,
+      token,
+    });
+  },
+
+  async save(token: string, params: { ids: string[] }, isThrowError = false) {
+    return fetcher<void>(
+      endpoint,
+      {
+        method: 'PUT',
+        body: JSON.stringify(params),
+      },
+      {
         isThrowError,
         token,
-      });
-    },
+      },
+    );
+  },
 
-    async save(token: string, params: { ids: string[] }, isThrowError = false) {
-      return fetcher<void>(
-        endpoint,
-        {
-          method: 'PUT',
-          body: JSON.stringify(params),
-        },
-        {
-          isThrowError,
-          token,
-        },
-      );
-    },
-
-    async delete(token: string, params: { ids: string[] }, isThrowError = false) {
-      return fetcher<void>(
-        endpoint,
-        {
-          method: 'DELETE',
-          body: JSON.stringify(params),
-        },
-        {
-          isThrowError,
-          token,
-        },
-      );
-    },
-
-    async contain(token: string, params: { ids: string[] }, isThrowError = false) {
-      const ids = params.ids.join(',');
-      const url = pathBuilder(endpoint, { ids });
-      return fetcher<MeAlbumsContainModel>(url, undefined, {
-        validationSchema: meAlbumsContainModelSchema,
+  async delete(token: string, params: { ids: string[] }, isThrowError = false) {
+    return fetcher<void>(
+      endpoint,
+      {
+        method: 'DELETE',
+        body: JSON.stringify(params),
+      },
+      {
         isThrowError,
         token,
-      });
-    },
-  };
+      },
+    );
+  },
+
+  async contain(token: string, params: { ids: string[] }, isThrowError = false) {
+    const ids = params.ids.join(',');
+    const url = pathBuilder(endpoint, { ids });
+    return fetcher<MeAlbumsContainModel>(url, undefined, {
+      validationSchema: meAlbumsContainModelSchema,
+      isThrowError,
+      token,
+    });
+  },
 };
 
-export type MeAlbumsRepository = ReturnType<typeof meAlbumsRepository>;
+export type MeAlbumsRepository = typeof meAlbumsRepository;
--- src/feature/me/use-case/index.ts
+++ src/feature/me/use-case/index.ts
@@ -3,4 +3,4 @@ import { mePlaylistRepository } from '@/domain/mePlaylists/mePlaylistsRepository
 import { meInteractor as interactor } from './interactor';
 
 export { type MeViewModel, type MePlaylistViewModel } from './boundary';
-export const meInteractor = interactor(meRepository(), mePlaylistRepository());
+export const meInteractor = interactor(meRepository, mePlaylistRepository);
