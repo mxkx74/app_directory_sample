1e0650091d78f12ee4d59fb289b1e77eacf48755
0x66ccff@gmail.com <0x66ccff@gmail.com>
2023-12-31 14:13:57 +0900

    Add albumsRepository and albumsModel files

---
+++ src/domain/album/albumsRepository.ts
@@ -0,0 +1,29 @@
+import { path } from '@/constant/path';
+import { fetcher } from '@/lib/fetcher';
+import { pathBuilder } from '@/util/path';
+import { albumsModelSchema, type AlbumsModel, type AlbumsListModel } from './albumsModel';
+
+const endpoint = path.albums;
+
+export const albumsRepository = {
+  async findByID(token: string, params: { id: string }, isThrowError = false) {
+    const url = pathBuilder(endpoint, params);
+    return fetcher<AlbumsModel>(url, undefined, {
+      validationSchema: albumsModelSchema,
+      isThrowError,
+      token,
+    });
+  },
+
+  async findList(token: string, params: { ids: string[] }, isThrowError = false) {
+    const ids = params.ids.join(',');
+    const url = pathBuilder(endpoint, { ids });
+    return fetcher<AlbumsListModel>(url, undefined, {
+      validationSchema: albumsModelSchema,
+      isThrowError,
+      token,
+    });
+  },
+};
+
+export type AlbumsRepository = typeof albumsRepository;
