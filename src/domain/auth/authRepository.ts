1d8e5f4bfcdde03fc7f19299d8644b451b71afe0
0x66ccff@gmail.com <0x66ccff@gmail.com>
2024-01-03 16:10:51 +0900

    Refactor authRepository and authInteractor

--- src/domain/auth/authRepository.ts
+++ src/domain/auth/authRepository.ts
@@ -2,44 +2,41 @@ import { signIn, signOut } from 'next-auth/react';
 import { path } from '@/constant/path';
 import { authModelSchema, type AuthModel } from '@/domain/auth/authModel';
 import { fetcher } from '@/lib/fetcher';
+const endpoint = path.auth.token;
 
-export const authRepository = () => {
-  const endpoint = path.auth.token;
+export const authRepository = {
+  // ユーザーがログインした後、callbackUrlにリダイレクトされます。
+  signIn(callbackUrl = '/') {
+    void signIn('spotify', { callbackUrl });
+  },
 
-  return {
-    // ユーザーがログインした後、callbackUrlにリダイレクトされます。
-    signIn(callbackUrl = '/') {
-      void signIn('spotify', { callbackUrl });
-    },
+  // ユーザーログアウトします。
+  signOut() {
+    void signOut();
+  },
 
-    // ユーザーログアウトします。
-    signOut() {
-      void signOut();
-    },
+  // アクセストークンを更新し、更新されたトークンを返します。
+  async refreshAccessToken(refreshToken?: string) {
+    const body = new URLSearchParams({
+      client_id: process.env.SPOTIFY_CLIENT_ID as string,
+      client_secret: process.env.SPOTIFY_CLIENT_SECRET as string,
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken ?? '',
+    });
 
-    // アクセストークンを更新し、更新されたトークンを返します。
-    async refreshAccessToken(refreshToken?: string) {
-      const body = new URLSearchParams({
-        client_id: process.env.SPOTIFY_CLIENT_ID as string,
-        client_secret: process.env.SPOTIFY_CLIENT_SECRET as string,
-        grant_type: 'refresh_token',
-        refresh_token: refreshToken ?? '',
-      });
-
-      return fetcher<AuthModel>(
-        endpoint,
-        {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          method: 'POST',
-          cache: 'no-store',
-          body,
-        },
-        {
-          validationSchema: authModelSchema,
-        },
-      );
-    },
-  };
+    return fetcher<AuthModel>(
+      endpoint,
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        method: 'POST',
+        cache: 'no-store',
+        body,
+      },
+      {
+        validationSchema: authModelSchema,
+      },
+    );
+  },
 };
 
-export type AuthRepository = ReturnType<typeof authRepository>;
+export type AuthRepository = typeof authRepository;
--- src/feature/auth/use-case/index.ts
+++ src/feature/auth/use-case/index.ts
@@ -1,5 +1,5 @@
 import { authRepository } from '@/domain/auth/authRepository';
 import { authInteractor as interactor } from './interactor';
 
-export const authInteractor = interactor(authRepository());
+export const authInteractor = interactor(authRepository);
 export { type AuthViewModel } from './boundary';
