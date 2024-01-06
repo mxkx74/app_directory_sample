53083dfafaf889f75a810bb02e22dc5c2b6c00dd
0x66ccff@gmail.com <0x66ccff@gmail.com>
2024-01-06 22:46:04 +0900

    Remove form and search functionality from Home page

--- src/component/page/Home/Home.tsx
+++ src/component/page/Home/Home.tsx
@@ -1,14 +1,3 @@
-import React from 'react';
-import { Button, Input, Stack } from '@/component/ui';
-import styles from './Home.module.scss';
-
 export const HomePage = () => {
-  return (
-    <form className={styles.form}>
-      <Stack direction="row" className={styles.search}>
-        <Input type="text" />
-        <Button type="submit">Submit</Button>
-      </Stack>
-    </form>
-  );
+  return null;
 };
