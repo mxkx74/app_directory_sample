acf667e178957bdc5820c7b1cf6bfda566e756f3
0x66ccff@gmail.com <0x66ccff@gmail.com>
2024-01-08 15:42:37 +0900

    Add date-fns library and update Album component

--- src/feature/albumsInfo/component/AlbumInfo.tsx
+++ src/feature/albumsInfo/component/AlbumInfo.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { intervalToDuration } from 'date-fns';
 import Image from 'next/image';
 import { getServerSession } from 'next-auth';
 import { Avatar, AvatarFallback, AvatarImage, Stack } from '@/component/ui';
@@ -7,14 +9,19 @@ import styles from './AlbumInfo.module.scss';
 
 type Props = AlbumsInfoViewModel;
 
-export const AlbumInfoPresentation = (props: Props) => {
-  const [img] = props.images;
-  const [artist] = props.artists;
+export const AlbumInfoPresentation = ({
+  images,
+  artists,
+  total_duration_ms,
+  name,
+  release_date,
+  total_tracks,
+}: Props) => {
+  const [img] = images;
+  const [artist] = artists;
   const [artistImage] = artist.images ?? [];
-  const { total_duration_ms: ms } = props;
-
-  const hour = Math.floor(ms / 3600000);
-  const min = Math.floor((ms % 3600000) / 60000);
+  const { hours, minutes, seconds } = intervalToDuration({ start: 0, end: total_duration_ms });
+  const defaultCoverWidth = 128;
 
   return (
     <Stack space="L" className={styles.wrapper} direction="row">
@@ -22,9 +29,9 @@ export const AlbumInfoPresentation = (props: Props) => {
         {img.url && (
           <Image
             src={img.url}
-            alt={props.name ?? ''}
-            width={img.width ?? 128}
-            height={img.height ?? 128}
+            alt={name ?? ''}
+            width={img.width ?? defaultCoverWidth}
+            height={img.height ?? defaultCoverWidth}
             aria-hidden="true"
             loading="lazy"
           />
@@ -32,16 +39,16 @@ export const AlbumInfoPresentation = (props: Props) => {
       </div>
       <Stack space="S" className={styles.info}>
         <span>アルバム</span>
-        <h1 className={styles.name}>{props.name}</h1>
+        <h1 className={styles.name}>{name}</h1>
         <Stack space="none" className={styles.artist} direction="row">
           <Avatar size="sm" rounded="full" className={styles.avatar}>
             {artistImage && <AvatarImage alt={artist.name} src={artistImage.url} loading="lazy" />}
             <AvatarFallback>{artist.name?.substring(0, 2).toUpperCase()}</AvatarFallback>
           </Avatar>
           <span>{artist.name}</span>
-          <span>{props.release_date}</span>
+          <span>{release_date}</span>
           <span>
-            {props.total_tracks}曲, {`${hour}時間 ${min}分`}
+            {total_tracks}曲, {hours && `${hours}時間`} {minutes && `${minutes}分`} {seconds && `${seconds}秒`}
           </span>
         </Stack>
       </Stack>
